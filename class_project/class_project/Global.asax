5c3c8f9561a44cdaf5a4d48674ef2926c9d10a04
gibsgibs-laptop <gbrehm@hotmail.com>
2020-01-21 16:42:05 -0800

    Created the blank project with user logins integrated.

---
+++ class_project/class_project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="class_project.MvcApplication" Language="C#" %>
---
+++ class_project/class_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(class_project.Startup))]
+namespace class_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
