9e10d68a0a93e70a790304c5009aec866d1ba423
Jensen Almquist <jensen5997@gmail.com>
2020-02-01 17:37:53 -0800

    Created controller and views for viewing and deleting users as an admin

---
+++ class_project/class_project/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using class_project.Models;
+
+namespace class_project.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Users.ToList());
+        }
+
+        public ActionResult Delete(string id)
+        {
+            ApplicationUser user = context.Users.Where(x => x.Id == id).FirstOrDefault();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(ApplicationUser userToDelete)
+        {
+            ApplicationUser user = context.Users.Where(x => x.Id == userToDelete.Id).FirstOrDefault();
+            context.Users.Remove(user);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
