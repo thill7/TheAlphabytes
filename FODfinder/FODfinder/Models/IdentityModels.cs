d3b1470bc40c80d90db0cba56410f9f6ee2bdad9
gibsgibs-desktop <g.brehm@hotmail.com>
2020-02-19 22:56:14 -0800

    Tried something different for continuous deployment on dev.

--- FODfinder/FODfinder/Models/IdentityModels.cs
+++ FODfinder/FODfinder/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace FODfinder.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("FFDBContext_Azure_test", throwIfV1Schema: false)
+            : base("FFDBContext_Azure_Dev", throwIfV1Schema: false)
         {
         }
 
