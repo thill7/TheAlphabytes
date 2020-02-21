aadd7b5d4395c2a77ded69b725b8199bb8db6f49
Tanner Hill <thill17@wou.edu>
2020-02-21 13:14:31 -0800

    added dynamic food label and fixed minor styling issues in food details page

--- FODfinder/FODfinder/Scripts/React/Food/FoodDetails.jsx
+++ FODfinder/FODfinder/Scripts/React/Food/FoodDetails.jsx
@@ -7,20 +7,98 @@
         };
     }
 
+    GetFoodNutrientValue(key) {
+        if (key != undefined) {
+            return key.value;
+        }
+        return "";
+    }
+
+    componentDidMount() {
+        var { details } = this.state;
+        var labelNutrients = JSON.parse(details.LabelNutrients);
+
+        $('#nutritionLabel').nutritionLabel({
+            allowCustomWidth: true,
+            allowNoBorder: true,
+            showServingUnitQuantity: true,
+            itemName: details.Description,
+            ingredientList: details.Ingredients,
+            showCalories: labelNutrients.calories != undefined,
+            showFatCalories: false,
+            showTotalFat: labelNutrients.fat != undefined,
+            showSatFat: labelNutrients.saturatedFat != undefined,
+            showTransFat: labelNutrients.transFat != undefined,
+            showPolyFat: labelNutrients.polyFat != undefined,
+            showMonoFat: labelNutrients.monoFat != undefined,
+            showCholesterol: labelNutrients.cholesterol != undefined,
+            showSodium: labelNutrients.sodium != undefined,
+            showPotassium_2018: labelNutrients.potassium != undefined,
+            showTotalCarb: labelNutrients.carbohydrates != undefined,
+            showFibers: labelNutrients.fiber != undefined,
+            showSugars: labelNutrients.sugars != undefined,
+            showAddedSugars: false,
+            showProteins: labelNutrients.protein != undefined,
+            showVitaminA: labelNutrients.vitaminA != undefined,
+            showVitaminC: labelNutrients.vitaminC != undefined,
+            showVitaminD: labelNutrients.vitaminD != undefined,
+            showCalcium: labelNutrients.calcium != undefined,
+            showIron: labelNutrients.iron != undefined,
+            showCaffeine: labelNutrients.caffeine != undefined,
+            valueCalories: this.GetFoodNutrientValue(labelNutrients.calories),
+            valueTotalFat: this.GetFoodNutrientValue(labelNutrients.fat),
+            valueSatFat: this.GetFoodNutrientValue(labelNutrients.saturatedFat),
+            valueTransFat: this.GetFoodNutrientValue(labelNutrients.transFat),
+            valuePolyFat: this.GetFoodNutrientValue(labelNutrients.polyFat),
+            valueMonoFat: this.GetFoodNutrientValue(labelNutrients.monoFat),
+            valueCholesterol: this.GetFoodNutrientValue(labelNutrients.cholesterol),
+            valueSodium: this.GetFoodNutrientValue(labelNutrients.sodium),
+            valueTotalCarb: this.GetFoodNutrientValue(labelNutrients.carbohydrates),
+            valueFibers: this.GetFoodNutrientValue(labelNutrients.fiber),
+            valueSugars: this.GetFoodNutrientValue(labelNutrients.sugars),
+            valueProteins: this.GetFoodNutrientValue(labelNutrients.protein),
+            valueVitaminA: this.GetFoodNutrientValue(labelNutrients.vitaminA),
+            valueVitaminC: this.GetFoodNutrientValue(labelNutrients.vitaminC),
+            valueVitaminD: this.GetFoodNutrientValue(labelNutrients.vitaminD),
+            valuePotassium_2018: this.GetFoodNutrientValue(labelNutrients.potassium),
+            valueCalcium: this.GetFoodNutrientValue(labelNutrients.calcium),
+            valueIron: this.GetFoodNutrientValue(labelNutrients.iron),
+            valueCaffeine: this.GetFoodNutrientValue(labelNutrients.caffeine),
+            valueServingSizeUnit: details.ServingSizeUnit,
+            valueServingUnitQuantity: details.ServingSize,
+            showLegacyVersion: false
+        });
+    }
+
     render() {
         var { details } = this.state;
+        
 
         return (
             <div className="pt-4">
                 <div className="card bg-secondary text-white shadow">
                     <div className="card-header">
-                        <h2 className="display-4 font-weight-normal">{details.Description}</h2>
+                        <h2 className="display-4 font-weight-normal text-capitalize">{details.Description.toLowerCase()}</h2>
                         <h3 className="font-weight-light">{details.BrandOwner}</h3>
                     </div>
                     <div className="card-body">
-                        <p className="text-lowercase"><span className="font-weight-bold text-capitalize">Ingredients:</span> {details.Ingredients}</p>
-                        <p><span className="font-weight-bold">UPC:</span> {details.UPC}</p>
-                        <p><span className="font-weight-bold">Serving Size:</span> {details.ServingSize}{details.ServingSizeUnit}</p>
+                        <div className="row">
+                            <div className="col-md-6 d-inline-flex flex-column justify-content-start align-items-start">
+                                <p className="text-lowercase"><span className="font-weight-bold text-capitalize">Ingredients:</span> {details.Ingredients}</p>
+                                
+                                <p className="d-inline-block"><span className="font-weight-bold">UPC:</span> {details.UPC}</p>
+                                <p><span className="font-weight-bold">Serving Size:</span> {details.ServingSize}{details.ServingSizeUnit}</p>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="shadow rounded bg-dark p-4">
+                                    <div id="nutritionLabel">
+
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
