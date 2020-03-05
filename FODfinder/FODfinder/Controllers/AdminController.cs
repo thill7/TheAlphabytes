a515eb4adfa7109a3986f9f81b696ba7669c7ba0
Jensen Almquist <jensen5997@gmail.com>
2020-03-04 20:25:16 -0800

    Created Admin controller with methods for viewing and deleting users

---
+++ FODfinder/FODfinder/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using FODfinder.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FODfinder.Controllers
+{
+    [Authorize(Roles = "Admin,SuperAdmin")]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            List<ApplicationUser> users = context.Users.ToList();
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            users.RemoveAll(x => userManager.IsInRole(x.Id, "Admin"));
+
+            return View(users);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            ApplicationUser user = context.Users.Where(x => x.Id == id).FirstOrDefault();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(ApplicationUser userToDelete)
+        {
+            ApplicationUser user = context.Users.Where(x => x.Id == userToDelete.Id).FirstOrDefault();
+            context.Users.Remove(user);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
