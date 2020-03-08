c9f66de442133d44e71fef35e5bd254d2acfe79c
gibsgibs-desktop <g.brehm@hotmail.com>
2020-03-07 20:33:16 -0800

    Added most of the functionality accept showing the lists of foods included and exculded.

--- FODfinder/FODfinder/Scripts/React/Food/FoodSearch.jsx
+++ FODfinder/FODfinder/Scripts/React/Food/FoodSearch.jsx
@@ -4,13 +4,22 @@
         this.state = {
             query: "",
             isUpc: false,
-            requireAllWords: false
+            requireAllWords: false,
+            isCollapsed: true,
+            includeItem: "",
+            includeList: [],
+            excludeItem: "",
+            excludeList: []
         };
 
         this.onQueryChanged.bind(this);
         this.onQuerySubmit.bind(this);
         this.onUpcToggle.bind(this);
         this.onRequireAllWords(this);
+        this.onCollapse(this);
+        this.addToIncludeList(this);
+        this.addToExcludeList(this);
+        this.showExcludeList(this);
     }
 
     async onQueryChanged(event) {
@@ -25,6 +34,44 @@
         await this.setState({ requireAllWords: toggled })
     }
 
+    async onCollapse(toggled) {
+        await this.setState({ isCollapsed: toggled })
+    }
+
+    async onAddInclude(item) {
+        await this.setState({ includeItem: item.target.value })
+    }
+
+    async addToIncludeList() {
+        if (!this.state.includeList.includes(this.state.includeItem)) {
+            await this.state.includeList.push(this.state.includeItem);
+        }
+        console.log("Include list: " + this.state.includeList)
+    }
+
+    async showExcludeList() {
+        if (this.state.excludeList.length != 0) {
+            return (
+                <div className="shadow rounded">
+                    <ul>
+                        this.state.excludeList.map(item => (<li key={item}>{item}</li>))
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    async onAddExclude(item) {
+        await this.setState({ excludeItem: item.target.value })
+    }
+
+    async addToExcludeList() {
+        if (!this.state.excludeList.includes(this.state.excludeItem)) {
+            await this.state.excludeList.push(this.state.excludeItem);
+        }
+        console.log("Exclude list: " + this.state.excludeList)
+    }
+
     onQuerySubmit(event) {
         event.preventDefault();
         var { isUpc, query } = this.state;
@@ -35,10 +82,9 @@
     }
 
     render() {
-        var { isUpc } = this.state;
-
+        var { isUpc, requireAllWords, isCollapsed } = this.state;
         return (
-            <div className="container-fluid px-0">
+            <div className="container px-0">
                 <form className="form w-100" onSubmit={(e) => { this.onQuerySubmit(e) }}>
                     <div className="form-group w-100 mb-0">
                         <div className="input-group w-100">
@@ -50,9 +96,9 @@
                             <input type={(!isUpc ? "text" : "number")} required={true} onChangeCapture={(e) => { this.onQueryChanged(e) }} className="form-control border-left-0 border-right-0" />
                             <div className="input-group-append">
                                 <div className="btn-group">
-                                    <button type="button" className="btn btn-block btn-primary rounded-0" data-toggle="collapse" data-target="#CollapseFilter">
+                                    <button type="button" className="btn btn-block btn-primary rounded-0" data-toggle="collapse" data-target="#CollapseFilter" onClick={ () => this.onCollapse(!isCollapsed) }>
                                         <span className="small">
-                                            ▼
+                                            {isCollapsed ? "▼" : "▲"}
                                         </span>
                                     </button>
                                     <button className="btn btn-primary" type="submit">Go</button>
@@ -62,30 +108,78 @@
                     </div>
                 </form>
                 <div className="collapse" id="CollapseFilter">
-                    <div className="card card-body shadow bg-secondary border-0">
-                        <div className="dropdown py-2">
-                            <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" id="SearchByButton">
-                                {"Search by " + (!isUpc ? "Name" : "UPC")}
-                            </button>
-                            <div className="dropdown-menu bg-secondary" aria-labelledby="SearchByButton">
-                                <a href="#" className={"dropdown-item text-gray" + (!isUpc ? " active bg-primary" : "")} onClick={() => { this.onUpcToggle(false) }}>
-                                    Search by Name
-                                </a>
-                                <a href="#" className={"dropdown-item text-gray" + (isUpc ? " active bg-primary" : "")} onClick={() => { this.onUpcToggle(true) }}>
-                                    Search by UPC
-                                </a>
+                    <div className="card card-body shadow bg-secondary">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-lg-6">
+                                <div className="input-group py-2">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">
+                                            Search by
+                                        </span>
+                                    </div>
+                                    <div className="input-group-append">
+                                        <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" id="SearchByButton">
+                                            {!isUpc ? "Name" : "UPC"}
+                                        </button>
+                                        <div className="dropdown-menu bg-secondary" aria-labelledby="SearchByButton">
+                                            <a href="#" className={"dropdown-item text-gray" + (!isUpc ? " active bg-primary" : "")} onClick={() => { this.onUpcToggle(false) }}>
+                                                Name
+                                            </a>
+                                            <a href="#" className={"dropdown-item text-gray" + (isUpc ? " active bg-primary" : "")} onClick={() => { this.onUpcToggle(true) }}>
+                                                UPC
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="input-group py-2">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">
+                                            Exact phrase search
+                                        </span>
+                                    </div>
+                                    <div className="input-group-append">
+                                        <button className={"btn " + (isUpc ? "btn-danger" : !requireAllWords ? "btn-danger" : "btn-success")} disabled={isUpc ? "disabled" : ""} onClick={() => { this.onRequireAllWords(!requireAllWords) }}>
+                                            {isUpc ? "Disabled" : !requireAllWords ? "Disabled" : "Enabled"}
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="input-group py-2">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"> 
-                                    Exact phrase search
-                                </span>
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-lg-6">
+                                <div className="input-group py-2">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">
+                                            Include
+                                        </span>
+                                    </div>
+                                    <input type="text" className="form-control" disabled={isUpc ? "disabled" : ""} onChangeCapture={(e) => { this.onAddInclude(e) }} />
+                                    <div className="input-group-append">
+                                        <button className="btn btn-primary" disabled={isUpc ? "disabled" : ""} onClick={() => this.addToIncludeList()}>
+                                            Add
+                                        </button>
+                                    </div>
+                                </div>
+                                <div>
+                                    {this.showExcludeList()}
+                                </div>
                             </div>
-                            <div className="input-group-append">
-                                <button className={!this.state.requireAllWords ? "btn btn-danger" : "btn btn-success"} onClick={() => { !this.state.requireAllWords ? this.onRequireAllWords(true) : this.onRequireAllWords(false) }}>
-                                    {!this.state.requireAllWords ? "Disabled" : "Enabled"}
-                                </button>
+                            <div className="col-lg-6">
+                                <div className="input-group py-2">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">
+                                            Exclude
+                                        </span>
+                                    </div>
+                                    <input type="text" className="form-control" disabled={isUpc ? "disabled" : ""} onChangeCapture={(e) => { this.onAddExclude(e) }} />
+                                    <div className="input-group-append">
+                                        <button type="submit" className="btn btn-primary" disabled={isUpc ? "disabled" : ""} onClick={() => this.addToExcludeList()}>
+                                            Add
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
