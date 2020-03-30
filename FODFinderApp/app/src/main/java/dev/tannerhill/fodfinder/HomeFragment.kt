a95b0b87b22e507ee603fd539ae94df846a3cdb8
Tanner Hill <thill17@wou.edu>
2020-03-30 12:56:46 -0700

    initial setup and navigation drawer implemented

---
+++ FODFinderApp/app/src/main/java/dev/tannerhill/fodfinder/HomeFragment.kt
@@ -0,0 +1,35 @@
+package dev.tannerhill.fodfinder
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+    }
+}
