05e7dbd2e3172b8192b469b4a6044049f66cc5fe
Tanner Hill <Tanner.Hill@insightsnow.com>
2020-04-01 12:28:14 -0700

    added logo to home and removed old info pages

--- FODFinderApp/app/src/main/java/dev/tannerhill/fodfinder/HomeFragment.kt
+++ FODFinderApp/app/src/main/java/dev/tannerhill/fodfinder/HomeFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +24,10 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onAttach(context: Context) {
