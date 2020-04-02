c61d8e5e8ba3b95d48af1de735b1cac7f3944286
gibsgibs-desktop <g.brehm@hotmail.com>
2020-04-01 21:59:07 -0700

    Made some good progress. Moving away from the RegEx, and opting for more generic string manipulation.

--- FODfinder/FODfinder/Utility/IngredientParser.cs
+++ FODfinder/FODfinder/Utility/IngredientParser.cs
@@ -12,7 +12,9 @@ namespace FODfinder.Utility
         //@"(([\w\s-]+\([\w\s-,]+\))|[\w\s-]+)|((, contains|contains|, less|less|than|2|%|percent|of|or|the|following|;|\:|\s)+)|(([\w\s-]+\([\w\s-,]+\))|[\w\s-]+)"
         private const string OtherPattern = @"(([\w\s-]+\([\w\s-,]+\))|[\w\s-]+)|((, |; |\: )?(contains|less|than|2|%|percent|of|or|the|following|\s)+(;|\:))";
         private const string PATTERN = @"[\w\s']+";
-        private static readonly string[] ToRemove = { "contains less than 2% of", "ingredients", ":", "contains 2% or less of", "less than 2%", "less than 2 percent" };
+        private const string MultiIngredientPattern = @"[\w\s-]+(\([\w\s-,]+\))*";
+        private static readonly string[] ToRemove = { "ingredients", ":", ";", "made of", "." };
+        private static readonly string[] Variations = { "contains less than 2% of", "contains 2% or less of", "less than 2% of", "less than 2%", "less than 2 percent" };
 
         public static List<string> Parse(string ingredients)
         {
@@ -32,7 +34,25 @@ namespace FODfinder.Utility
             primaryIngredients = new List<string>();
             secondaryIngredients = new List<string>();
             ingredients = ingredients.ToLower();
-            var matches = Regex.Matches(ingredients, OtherPattern);
+            foreach (var toRemove in ToRemove)
+            {
+                ingredients = ingredients.Replace(toRemove, "");
+            }
+            var index = -1;
+            var length = 0;
+            foreach (var item in Variations)
+            {
+                index = ingredients.IndexOf(item);
+                if(index != -1)
+                {
+                    length = item.Length;
+                    break;
+                }
+            }
+            var primaryIngredientsString = ingredients.Substring(0, index);
+            var secondaryIngredientsString = ingredients.Substring(index + length);
+            var primaryMatches = Regex.Matches(primaryIngredientsString, MultiIngredientPattern);
+            var secondaryMatches = Regex.Matches(secondaryIngredientsString, MultiIngredientPattern);
         }
     }
 }
