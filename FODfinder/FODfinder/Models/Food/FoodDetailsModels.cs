d2d5d3f148a63230fd04a9b45b7da1c665c0e29e
gibsgibs-desktop <g.brehm@hotmail.com>
2020-04-02 21:40:06 -0700

    Added some functionallity to the FoodDetailsModel to deal with the two lists.

--- FODfinder/FODfinder/Models/Food/FoodDetailsModels.cs
+++ FODfinder/FODfinder/Models/Food/FoodDetailsModels.cs
@@ -13,8 +13,8 @@ namespace FODfinder.Models.Food
         public string Description { private set; get; }
         public string BrandOwner { private set; get; }
         public List<Ingredient> Ingredients { private set; get; } = new List<Ingredient>();
-        public List<Ingredient> PrimaryIngredients { private set; get; } = new List<Ingredient>();
-        public List<Ingredient> SecondaryIngredients { private set; get; } = new List<Ingredient>();
+        public List<List<Ingredient>> PrimaryIngredients { private set; get; } = new List<List<Ingredient>>();
+        public List<List<Ingredient>> SecondaryIngredients { private set; get; } = new List<List<Ingredient>>();
         public double ServingSize { private set; get; }
         public string ServingSizeUnit { private set; get; }
         public string LabelNutrients { private set; get; }
@@ -27,21 +27,63 @@ namespace FODfinder.Models.Food
             var ingredientString = detailObject.SelectToken("ingredients")?.ToString() ?? "";
             if(!string.IsNullOrEmpty(ingredientString))
             {
-                var primary = new List<List<string>>();
-                var secondary = new List<List<string>>();
-                IngredientParser.Parse(ingredientString, out primary, out secondary);
-
-                var parsedIngredients = IngredientParser.Parse(ingredientString);
-                foreach(var ingredient in parsedIngredients)
+                var primaryListofLists = new List<List<string>>();
+                var secondaryListofLists = new List<List<string>>();
+                IngredientParser.Parse(ingredientString, out primaryListofLists, out secondaryListofLists);
+                foreach (var ingredient in primaryListofLists)
                 {
-                    if(string.IsNullOrEmpty(ingredient))
+                    if(ingredient.Count > 1)
                     {
-                        continue;
+                        List<Ingredient> list = new List<Ingredient>();
+                        foreach (var subIngredient in ingredient)
+                        {
+                            var fodmap = db.FODMAPIngredients.Where(f => subIngredient.Contains(f.Name.ToLower())).FirstOrDefault();
+                            list.Add(new Ingredient(subIngredient, fodmap != null));
+                        }
+                        PrimaryIngredients.Add(list);
                     }
-                    var fodmap = db.FODMAPIngredients.Where(f => ingredient.Contains(f.Name.ToLower())).FirstOrDefault();
-                    Ingredients.Add(new Ingredient(ingredient, fodmap != null));
+                    else if (ingredient.Count == 1)
+                    {
+                        var fodmap = db.FODMAPIngredients.Where(f => ingredient.Contains(f.Name.ToLower())).FirstOrDefault();
+                        PrimaryIngredients.Add(new List<Ingredient>() {new Ingredient(ingredient.FirstOrDefault(), fodmap != null) });
+                    }
+                }
+                foreach (var ingredient in secondaryListofLists)
+                {
+                    if (ingredient.Count > 1)
+                    {
+                        List<Ingredient> list = new List<Ingredient>();
+                        foreach (var subIngredient in ingredient)
+                        {
+                            var fodmap = db.FODMAPIngredients.Where(f => subIngredient.Contains(f.Name.ToLower())).FirstOrDefault();
+                            list.Add(new Ingredient(subIngredient, fodmap != null));
+                        }
+                        SecondaryIngredients.Add(list);
+                    }
+                    else if (ingredient.Count == 1)
+                    {
+                        var fodmap = db.FODMAPIngredients.Where(f => ingredient.Contains(f.Name.ToLower())).FirstOrDefault();
+                        SecondaryIngredients.Add(new List<Ingredient>() { new Ingredient(ingredient.FirstOrDefault(), fodmap != null) });
+                    }
                 }
             }
+            //if(!string.IsNullOrEmpty(ingredientString))
+            //{
+            //    var primary = new List<List<string>>();
+            //    var secondary = new List<List<string>>();
+            //    IngredientParser.Parse(ingredientString, out primary, out secondary);
+
+            //    var parsedIngredients = IngredientParser.Parse(ingredientString);
+            //    foreach(var ingredient in parsedIngredients)
+            //    {
+            //        if(string.IsNullOrEmpty(ingredient))
+            //        {
+            //            continue;
+            //        }
+            //        var fodmap = db.FODMAPIngredients.Where(f => ingredient.Contains(f.Name.ToLower())).FirstOrDefault();
+            //        Ingredients.Add(new Ingredient(ingredient, fodmap != null));
+            //    }
+            //}
             double servingSize;
             ServingSize = double.TryParse(detailObject.SelectToken("servingSize")?.ToString() ?? "", out servingSize) ? servingSize : 0.0;
             ServingSizeUnit = detailObject.SelectToken("servingSizeUnit")?.ToString() ?? "";
--- FODfinder/FODfinder/Utility/IngredientParser.cs
+++ FODfinder/FODfinder/Utility/IngredientParser.cs
@@ -13,7 +13,7 @@ namespace FODfinder.Utility
         private const string OtherPattern = @"(([\w\s-]+\([\w\s-,]+\))|[\w\s-]+)|((, |; |\: )?(contains|less|than|2|%|percent|of|or|the|following|\s)+(;|\:))";
         private const string PATTERN = @"[\w\s']+";
         private const string MultiIngredientPattern = @"[\w\s-]+(\([\w\s-,]+\))*";
-        private static readonly string[] ToRemove = { "ingredients", ":", ";", "made of", "." };
+        private static readonly string[] ToRemove = { "ingredients", ":", ";", "made of", ".", "contains one or more of the following" };
         private static readonly string[] Variations = { "contains less than 2% of", "contains 2% or less of", "less than 2% of", "less than 2%", "less than 2 percent" };
 
         public static List<string> Parse(string ingredients)
@@ -32,43 +32,36 @@ namespace FODfinder.Utility
         private static MatchCollection MatchRegEx(string ingredientsString) => Regex.Matches(ingredientsString, MultiIngredientPattern);
         private static IEnumerable<string> ConvertToEnumerable(MatchCollection matches) => matches.Cast<Match>().Select(m => $"{m}".Trim());
         private static List<string> ConvertToList(string ingredient) => ingredient.Contains('(') ? ingredient.Replace(")", "").Replace('(', ',').Split(',').ToList() : new List<string>() { ingredient };
-
+        private static List<List<string>> ConvertToListOfLists(IEnumerable<string> ingredients)
+        {
+            var listOfLists = new List<List<string>>();
+            ingredients.ToList().ForEach(i => listOfLists.Add(ConvertToList(i)));
+            return listOfLists;
+        }
         public static void Parse(string ingredients, out List<List<string>> primaryIngredients, out List<List<string>> secondaryIngredients)
         {
-            ingredients = ingredients.ToLower();
-            foreach (var toRemove in ToRemove)
-            {
-                ingredients = ingredients.Replace(toRemove, "");
-            }
+            ToRemove.ToList().ForEach(tr => ingredients = ingredients.ToLower().Replace(tr, ""));
             var index = -1;
             var length = 0;
             foreach (var item in Variations)
             {
-                index = ingredients.IndexOf(item);
-                if(index != -1)
+                if((index = ingredients.IndexOf(item)) != -1)
                 {
                     length = item.Length;
                     break;
                 }
             }
-            var primaryIngredientsString = ingredients.Substring(0, index);
-            var secondaryIngredientsString = ingredients.Substring(index + length);
-
-            var primaryIngredientsEnumerable = ConvertToEnumerable(MatchRegEx(primaryIngredientsString));
-            var secondaryIngredientsEnumerable = ConvertToEnumerable(MatchRegEx(secondaryIngredientsString));
-
-            primaryIngredients = new List<List<string>>();
-            secondaryIngredients = new List<List<string>>();
-
-            foreach (var primaryIngredient in primaryIngredientsEnumerable)
+            if (index != -1)
             {
-                var list = ConvertToList(primaryIngredient);
-                primaryIngredients.Add(list);
+                var primaryIngredientsString = ingredients.Substring(0, index);
+                var secondaryIngredientsString = ingredients.Substring(index + length);
+                secondaryIngredients = ConvertToListOfLists(ConvertToEnumerable(MatchRegEx(secondaryIngredientsString)));
+                primaryIngredients = ConvertToListOfLists(ConvertToEnumerable(MatchRegEx(primaryIngredientsString)));
             }
-            foreach (var secondaryIngredient in secondaryIngredientsEnumerable)
+            else
             {
-                var list = ConvertToList(secondaryIngredient);
-                secondaryIngredients.Add(list);
+                primaryIngredients = ConvertToListOfLists(ConvertToEnumerable(MatchRegEx(ingredients)));
+                secondaryIngredients = null;
             }
         }
     }
