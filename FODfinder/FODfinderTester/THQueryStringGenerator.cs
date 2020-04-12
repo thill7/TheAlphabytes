e2f4309e41b2cb58c06a531e5ab04ca1aca5e182
Tanner Hill <Tanner.Hill@insightsnow.com>
2020-04-11 17:19:20 -0700

    abstracted GenerateQueryString function and created tests

---
+++ FODfinder/FODfinderTester/THQueryStringGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FODfinder.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FODfinderTester
+{
+    [TestClass]
+    public class THQueryStringGenerator
+    {
+        private FoodController _foodController;
+        [TestInitialize]
+        public void Init()
+        {
+            _foodController = new FoodController();
+        }
+
+        [TestMethod]
+        public void GenerateQueryString_CanCombineMultipleKeyValuePairs()
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>()
+            {
+                { "user","tanner"},
+                {"age","23" }
+            };
+            string result = _foodController.GenerateQueryString(parameters);
+            StringAssert.Equals("user=tanner&age=23", result);
+        }
+
+        [TestMethod]
+        public void GenerateQueryString_WillEncodeUrlEntities()
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>()
+            {
+                { "user","tanner hill"},
+                {"age","23" }
+            };
+            string result = _foodController.GenerateQueryString(parameters);
+            StringAssert.Equals("user=tanner+hill&age=23", result);
+        }
+    }
+}
