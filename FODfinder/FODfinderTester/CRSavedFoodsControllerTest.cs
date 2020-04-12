3d3af2fe03325093e0cc5f285753ebff8a3a147b
Cassidy Ryan Laptop <cassidy.lynn.ryan@gmail.com>
2020-04-12 13:59:40 -0700

    wrote two passing tests, ready for merge

---
+++ FODfinder/FODfinderTester/CRSavedFoodsControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using FODfinder.Controllers;
+using FODfinder.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FODfinderTester
+{
+    [TestClass]
+    public class CRSavedFoodsControllerTest
+    {
+        private SavedFoodsController _savedFoodsController;
+        [TestInitialize]
+        public void Init()
+        {
+            _savedFoodsController = new SavedFoodsController();
+        }
+        [TestMethod]
+        public void countSavedFoods_CanCount_Regular()
+        {
+            //Arrange
+            List<SavedFood> testList = new List<SavedFood>
+            {
+                new SavedFood { usdaFoodID = 1, userID = "35", brand = "brand name", desc = "Hello", upc = "003647898" },
+                new SavedFood { usdaFoodID = 1, userID = "35", brand = "brand name", desc = "Hello", upc = "003647898" },
+                new SavedFood { usdaFoodID = 1, userID = "35", brand = "brand name", desc = "Hello", upc = "003647898" }
+            };
+            int expectedResult = 3;
+
+            //Act
+            int actualResult = _savedFoodsController.countSavedFoods(testList);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    
+        [TestMethod]
+        public void countSavedFoods_CanCount_Empty()
+        {
+            //Arrange
+            List<SavedFood> testList = new List<SavedFood>();
+            int expectedResult = 0;
+
+            //Act
+            int actualResult = _savedFoodsController.countSavedFoods(testList);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
