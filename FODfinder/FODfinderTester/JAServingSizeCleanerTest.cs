ba6177967e6554620731455071c139a72739414f
Jensen Almquist <jensen5997@gmail.com>
2020-04-12 17:21:46 -0700

    Created test class for ServingSizeCleaner helper class with 2 passing tests

---
+++ FODfinder/FODfinderTester/JAServingSizeCleanerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FODfinder.Utility;
+using System.Threading.Tasks;
+
+namespace FODfinderTester
+{
+    [TestClass]
+    public class JAServingSizeCleanerTest
+    {
+        //private ServingSizeCleaner _servingSizeCleaner;
+        [TestMethod]
+        public void CleanEmptyString_ReturnsEmpty()
+        {
+            string result = ServingSizeCleaner.Clean("");
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void CleanRegular_ReturnsLowerCase()
+        {
+            string result = ServingSizeCleaner.Clean("THIs is 1 teST!");
+            Assert.AreEqual("this is 1 test!", result);
+        }
+    }
+}
