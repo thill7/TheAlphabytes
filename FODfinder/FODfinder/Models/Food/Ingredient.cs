865ebf3ec90d9c06ea1ea281db5bf0b23c1d4f45
gibsgibs-desktop <g.brehm@hotmail.com>
2020-04-20 20:43:27 -0700

    Added a new bool property to the Ingredient class. Also added the configsource to the web.config.

--- FODfinder/FODfinder/Models/Food/Ingredient.cs
+++ FODfinder/FODfinder/Models/Food/Ingredient.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace FODfinder.Models.Food
+﻿namespace FODfinder.Models.Food
 {
     public class Ingredient
     {
         public string Name { get; private set; }
         public bool IsFodmap { get; private set; }
         public string Label { get; private set; }
+        public bool IsPartOfSublist { get; private set; }
 
-        public Ingredient(String name, bool isFodmap, string label)
+        public Ingredient(string name, bool isFodmap, string label, bool isPartOfSublist)
         {
-            this.Name = name;
-            this.IsFodmap = isFodmap;
-            this.Label = label;
+            Name = name;
+            IsFodmap = isFodmap;
+            Label = label;
             if (label == "Low-Risk")
             {
-                this.IsFodmap = false;
+                IsFodmap = false;
             }
+            IsPartOfSublist = isPartOfSublist;
         }
     }
 }
