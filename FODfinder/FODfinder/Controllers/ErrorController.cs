4237132db99a2de566ee9f2dd78a65e22139935f
Jensen Almquist <jensen5997@gmail.com>
2020-05-02 20:40:16 -0700

    Set up custom error routing for 404, 500, and default error in web config and built corresponding controller

---
+++ FODfinder/FODfinder/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FODfinder.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Default()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult InternalServer()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
