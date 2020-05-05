8842a6842601bcd6d904e12d5b1a081e5d158d5b
Jensen Almquist <jensen5997@gmail.com>
2020-05-04 20:29:26 -0700

    Added plot to page with dummy data

---
+++ FODfinder/FODfinder/Scripts/barPlot.js
@@ -0,0 +1,14 @@
+﻿var data = [{
+    type: 'bar',
+    x: [2, 10, 2],
+    y: ['a ', 'b ', 'c '],
+    orientation: 'h'
+}];
+
+var layout = {
+    plot_bgcolor: "#F5EEEB",
+    paper_bgcolor: "#F5EEEB"
+}
+
+var chart = document.getElementById('chart');
+Plotly.newPlot(chart, data, layout);
