afe8789b4f463cf8f4740868d698d2e6f7107cd2
Jensen Almquist <jensen5997@gmail.com>
2020-05-04 22:51:26 -0700

    Graph is now populated by values from ajax call, still static, needs loop added

--- FODfinder/FODfinder/Scripts/barPlot.js
+++ FODfinder/FODfinder/Scripts/barPlot.js
@@ -1,14 +1,32 @@
-﻿var data = [{
-    type: 'bar',
-    x: [2, 10, 2],
-    y: ['a ', 'b ', 'c '],
-    orientation: 'h'
-}];
+﻿function ajaxCall() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetIngredientsForPlot',
+        success: successAjax,
+        error: errorAjax
+    })
+};
 
-var layout = {
-    plot_bgcolor: "#F5EEEB",
-    paper_bgcolor: "#F5EEEB"
+function successAjax(data) {
+    var data = [{
+        type: 'bar',
+        x: [data[0]['countOfLabelOccurences'], data[1]['countOfLabelOccurences'], data[2]['countOfLabelOccurences']],
+        y: [data[0]['ingredientName'], data[1]['ingredientName'], data[2]['ingredientName']],
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB'
+    }
+
+    var chart = document.getElementById('chart');
+    Plotly.newPlot(chart, data, layout);
 }
 
-var chart = document.getElementById('chart');
-Plotly.newPlot(chart, data, layout);
+function errorAjax() {
+    console.log("Error")
+}
+
+ajaxCall();
