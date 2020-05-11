469483f294d1003086a858cd5db6f0038d056d90
Jensen Almquist <jensen5997@gmail.com>
2020-05-10 18:18:05 -0700

    Created Donation page with link to PayPal and added link to navbar

---
+++ FODfinder/FODfinder/Controllers/DonateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FODfinder.Controllers
+{
+    public class DonateController : Controller
+    {
+        // GET: Donate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
