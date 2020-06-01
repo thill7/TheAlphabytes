e90c6bbfdcd85e99053fc537784d626eeb6c0808
Cassidy Ryan Desktop <cbullock18@wou.edu>
2020-05-31 20:16:44 -0700

    commit 2: added a script to make navigating the page much easier

---
+++ FODfinder/FODfinder/Scripts/Trends.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("#clickAge").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#age").offset().top
+        }, 2000);
+    });
+
+    $("#clickGender").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#gender").offset().top
+        }, 2000);
+    });
+
+    $("#clickEthnicity").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#ethnicity").offset().top
+        }, 2000);
+    });
+    $("#clickTop").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#top").offset().top
+        }, 2000);
+    });
+    $("#clickTop1").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#top").offset().top
+        }, 2000);
+    });
+    $("#clickTop2").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#top").offset().top
+        }, 2000);
+    });
+}); 
