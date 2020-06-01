0b42ba28458444300160546869d9c5e1a45f3925
Cassidy Ryan Desktop <cbullock18@wou.edu>
2020-05-31 20:21:45 -0700

    commit 3: added trends for different age groups

--- FODfinder/FODfinder/Controllers/HighRiskLabelledIngredientsController.cs
+++ FODfinder/FODfinder/Controllers/HighRiskLabelledIngredientsController.cs
@@ -34,6 +34,96 @@ namespace FODfinder.Controllers
             }
         }
 
+        public List<HighRiskLabelledIngredient> RetrieveRiskyIngredientsAge1()
+        {
+            using (FFDBContext db = new FFDBContext())
+            {
+                List<HighRiskLabelledIngredient> highRiskLabelledIngredientList = new List<HighRiskLabelledIngredient>();
+                List<int> highRiskList = db.UserIngredients.Where(u => u.Label == "High-Risk").Select(u => u.LabelledIngredientID).Distinct().ToList();
+                foreach (int ingredient in highRiskList)
+                {
+                    LabelledIngredient labelIng = db.LabelledIngredients.Where(l => l.ID == ingredient).FirstOrDefault();
+                    int count = db.UserIngredients.Where(u => u.LabelledIngredientID == ingredient && u.Label == "High-Risk" && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) < 18).Count();
+                    HighRiskLabelledIngredient newIngredient = new HighRiskLabelledIngredient(labelIng, count);
+                    highRiskLabelledIngredientList.Add(newIngredient);
+                }
+                highRiskLabelledIngredientList = highRiskLabelledIngredientList.OrderByDescending(o => o.countOfLabelOccurences).ToList();
+                return highRiskLabelledIngredientList;
+            }
+        }
+
+        public List<HighRiskLabelledIngredient> RetrieveRiskyIngredientsAge2()
+        {
+            using (FFDBContext db = new FFDBContext())
+            {
+                List<HighRiskLabelledIngredient> highRiskLabelledIngredientList = new List<HighRiskLabelledIngredient>();
+                List<int> highRiskList = db.UserIngredients.Where(u => u.Label == "High-Risk").Select(u => u.LabelledIngredientID).Distinct().ToList();
+                foreach (int ingredient in highRiskList)
+                {
+                    LabelledIngredient labelIng = db.LabelledIngredients.Where(l => l.ID == ingredient).FirstOrDefault();
+                    int count = db.UserIngredients.Where(u => u.LabelledIngredientID == ingredient && u.Label == "High-Risk" && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) > 17 && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) < 36).Count();
+                    HighRiskLabelledIngredient newIngredient = new HighRiskLabelledIngredient(labelIng, count);
+                    highRiskLabelledIngredientList.Add(newIngredient);
+                }
+                highRiskLabelledIngredientList = highRiskLabelledIngredientList.OrderByDescending(o => o.countOfLabelOccurences).ToList();
+                return highRiskLabelledIngredientList;
+            }
+        }
+
+        public List<HighRiskLabelledIngredient> RetrieveRiskyIngredientsAge3()
+        {
+            using (FFDBContext db = new FFDBContext())
+            {
+                List<HighRiskLabelledIngredient> highRiskLabelledIngredientList = new List<HighRiskLabelledIngredient>();
+                List<int> highRiskList = db.UserIngredients.Where(u => u.Label == "High-Risk").Select(u => u.LabelledIngredientID).Distinct().ToList();
+                foreach (int ingredient in highRiskList)
+                {
+                    LabelledIngredient labelIng = db.LabelledIngredients.Where(l => l.ID == ingredient).FirstOrDefault();
+                    int count = db.UserIngredients.Where(u => u.LabelledIngredientID == ingredient && u.Label == "High-Risk" && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) > 35 && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) < 56).Count();
+                    HighRiskLabelledIngredient newIngredient = new HighRiskLabelledIngredient(labelIng, count);
+                    highRiskLabelledIngredientList.Add(newIngredient);
+                }
+                highRiskLabelledIngredientList = highRiskLabelledIngredientList.OrderByDescending(o => o.countOfLabelOccurences).ToList();
+                return highRiskLabelledIngredientList;
+            }
+        }
+
+        public List<HighRiskLabelledIngredient> RetrieveRiskyIngredientsAge4()
+        {
+            using (FFDBContext db = new FFDBContext())
+            {
+                List<HighRiskLabelledIngredient> highRiskLabelledIngredientList = new List<HighRiskLabelledIngredient>();
+                List<int> highRiskList = db.UserIngredients.Where(u => u.Label == "High-Risk").Select(u => u.LabelledIngredientID).Distinct().ToList();
+                foreach (int ingredient in highRiskList)
+                {
+                    LabelledIngredient labelIng = db.LabelledIngredients.Where(l => l.ID == ingredient).FirstOrDefault();
+                    int count = db.UserIngredients.Where(u => u.LabelledIngredientID == ingredient && u.Label == "High-Risk" && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) > 55 && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) < 76).Count();
+                    HighRiskLabelledIngredient newIngredient = new HighRiskLabelledIngredient(labelIng, count);
+                    highRiskLabelledIngredientList.Add(newIngredient);
+                }
+                highRiskLabelledIngredientList = highRiskLabelledIngredientList.OrderByDescending(o => o.countOfLabelOccurences).ToList();
+                return highRiskLabelledIngredientList;
+            }
+        }
+
+        public List<HighRiskLabelledIngredient> RetrieveRiskyIngredientsAge5()
+        {
+            using (FFDBContext db = new FFDBContext())
+            {
+                List<HighRiskLabelledIngredient> highRiskLabelledIngredientList = new List<HighRiskLabelledIngredient>();
+                List<int> highRiskList = db.UserIngredients.Where(u => u.Label == "High-Risk").Select(u => u.LabelledIngredientID).Distinct().ToList();
+                foreach (int ingredient in highRiskList)
+                {
+                    LabelledIngredient labelIng = db.LabelledIngredients.Where(l => l.ID == ingredient).FirstOrDefault();
+                    int count = db.UserIngredients.Where(u => u.LabelledIngredientID == ingredient && u.Label == "High-Risk" && (DateTime.Now.Year - u.AspNetUser.UserInformation.birthdate.Year) > 75).Count();
+                    HighRiskLabelledIngredient newIngredient = new HighRiskLabelledIngredient(labelIng, count);
+                    highRiskLabelledIngredientList.Add(newIngredient);
+                }
+                highRiskLabelledIngredientList = highRiskLabelledIngredientList.OrderByDescending(o => o.countOfLabelOccurences).ToList();
+                return highRiskLabelledIngredientList;
+            }
+        }
+
         // GET: HighRiskLabelledIngredients
         public ActionResult Index()
         {
@@ -50,6 +140,35 @@ namespace FODfinder.Controllers
         {
             var highRiskLabelledIngredientList = RetrieveRiskyIngredients();
             return Json(highRiskLabelledIngredientList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAgeChart1()
+        {
+            var highRiskLabelledIngredientsAge1 = RetrieveRiskyIngredientsAge1();
+            return Json(highRiskLabelledIngredientsAge1, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetAgeChart2()
+        {
+            var highRiskLabelledIngredientsAge2 = RetrieveRiskyIngredientsAge2();
+            return Json(highRiskLabelledIngredientsAge2, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAgeChart3()
+        {
+            var highRiskLabelledIngredientsAge3 = RetrieveRiskyIngredientsAge3();
+            return Json(highRiskLabelledIngredientsAge3, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAgeChart4()
+        {
+            var highRiskLabelledIngredientsAge4 = RetrieveRiskyIngredientsAge4();
+            return Json(highRiskLabelledIngredientsAge4, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAgeChart5()
+        {
+            var highRiskLabelledIngredientsAge5 = RetrieveRiskyIngredientsAge5();
+            return Json(highRiskLabelledIngredientsAge5, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ FODfinder/FODfinder/Scripts/TrendPlots.js
@@ -0,0 +1,219 @@
+﻿function ajaxCall1() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetAgeChart1',
+        success: successAjax1,
+        error: errorAjax
+    })
+};
+
+function successAjax1(data) {
+    var xValues = [];
+    var yValues = [];
+    var textValues = [];
+    for (i = data.length - 1; i >= 0; i--) {
+        xValues.push(data[i]['countOfLabelOccurences']);
+        yValues.push(data[i]['ingredientName']);
+        textValues.push(data[i]['ingredientName']);
+    }
+
+    var data = [{
+        type: 'bar',
+        marker: { color: 'CCBDBD' },
+        x: xValues,
+        y: yValues,
+        text: textValues,
+        textposition: 'auto',
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB',
+        yaxis: { fixedrange: true, dtick: 1, visible: false },
+        xaxis: { fixedrange: true, dtick: 1 },
+        margin: { l: 130, t: 70, b: 70 }
+    }
+
+    var chart = document.getElementById('chartAgeIngredientsGroup1');
+    Plotly.newPlot(chart, data, layout);
+}
+
+function ajaxCall2() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetAgeChart2',
+        success: successAjax2,
+        error: errorAjax
+    })
+};
+
+function successAjax2(data) {
+    var xValues = [];
+    var yValues = [];
+    var textValues = [];
+    for (i = data.length - 1; i >= 0; i--) {
+        xValues.push(data[i]['countOfLabelOccurences']);
+        yValues.push(data[i]['ingredientName']);
+        textValues.push(data[i]['ingredientName']);
+    }
+
+    var data = [{
+        type: 'bar',
+        marker: { color: 'CCBDBD' },
+        x: xValues,
+        y: yValues,
+        text: textValues,
+        textposition: 'auto',
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB',
+        yaxis: { fixedrange: true, dtick: 1, visible: false },
+        xaxis: { fixedrange: true, dtick: 1 },
+        margin: { l: 130, t: 70, b: 70 }
+    }
+
+    var chart = document.getElementById('chartAgeIngredientsGroup2');
+    Plotly.newPlot(chart, data, layout);
+}
+
+function ajaxCall3() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetAgeChart3',
+        success: successAjax3,
+        error: errorAjax
+    })
+};
+
+function successAjax3(data) {
+    var xValues = [];
+    var yValues = [];
+    var textValues = [];
+    for (i = data.length - 1; i >= 0; i--) {
+        xValues.push(data[i]['countOfLabelOccurences']);
+        yValues.push(data[i]['ingredientName']);
+        textValues.push(data[i]['ingredientName']);
+    }
+
+    var data = [{
+        type: 'bar',
+        marker: { color: 'CCBDBD' },
+        x: xValues,
+        y: yValues,
+        text: textValues,
+        textposition: 'auto',
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB',
+        yaxis: { fixedrange: true, dtick: 1, visible: false },
+        xaxis: { fixedrange: true, dtick: 1 },
+        margin: { l: 130, t: 70, b: 70 }
+    }
+
+    var chart = document.getElementById('chartAgeIngredientsGroup3');
+    Plotly.newPlot(chart, data, layout);
+}
+
+function ajaxCall4() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetAgeChart4',
+        success: successAjax4,
+        error: errorAjax
+    })
+};
+
+function successAjax4(data) {
+    var xValues = [];
+    var yValues = [];
+    var textValues = [];
+    for (i = data.length - 1; i >= 0; i--) {
+        xValues.push(data[i]['countOfLabelOccurences']);
+        yValues.push(data[i]['ingredientName']);
+        textValues.push(data[i]['ingredientName']);
+    }
+
+    var data = [{
+        type: 'bar',
+        marker: { color: 'CCBDBD' },
+        x: xValues,
+        y: yValues,
+        text: textValues,
+        textposition: 'auto',
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB',
+        yaxis: { fixedrange: true, dtick: 1, visible: false },
+        xaxis: { fixedrange: true, dtick: 1 },
+        margin: { l: 130, t: 70, b: 70 }
+    }
+
+    var chart = document.getElementById('chartAgeIngredientsGroup4');
+    Plotly.newPlot(chart, data, layout);
+}
+
+function ajaxCall5() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: '/HighRiskLabelledIngredients/GetAgeChart5',
+        success: successAjax5,
+        error: errorAjax
+    })
+};
+
+function successAjax5(data) {
+    var xValues = [];
+    var yValues = [];
+    var textValues = [];
+    for (i = data.length - 1; i >= 0; i--) {
+        xValues.push(data[i]['countOfLabelOccurences']);
+        yValues.push(data[i]['ingredientName']);
+        textValues.push(data[i]['ingredientName']);
+    }
+
+    var data = [{
+        type: 'bar',
+        marker: { color: 'CCBDBD' },
+        x: xValues,
+        y: yValues,
+        text: textValues,
+        textposition: 'auto',
+        orientation: 'h'
+    }];
+
+    var layout = {
+        plot_bgcolor: '#F5EEEB',
+        paper_bgcolor: '#F5EEEB',
+        yaxis: { fixedrange: true, dtick: 1, visible: false },
+        xaxis: { fixedrange: true, dtick: 1 },
+        margin: { l: 130, t: 70, b: 70 }
+    }
+
+    var chart = document.getElementById('chartAgeIngredientsGroup5');
+    Plotly.newPlot(chart, data, layout);
+}
+
+function errorAjax() {
+    console.log("Error")
+}
+
+ajaxCall1();
+ajaxCall2();
+ajaxCall3();
+ajaxCall4();
+ajaxCall5(); 
